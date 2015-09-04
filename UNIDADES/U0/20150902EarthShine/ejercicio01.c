e7ddbdd9e9142292985aaea9cea2994e22d44836
Lamberto <lamberto@RyRII>
2015-09-04 17:32:04 -0600

    (2015.09.04.17.32)

---
+++ UNIDADES/U0/20150902EarthShine/ejercicio01.c
@@ -0,0 +1,9 @@
+int main(){
+  printf("%s\n", diadlasemana(20150904));
+  return 0;
+}
+/*
+El programa debera imprimir Lunes, Martes, Miercoles, etc. segun 
+el dia que corresponda a la fecha. La funcion debe funcionar bien 
+para cualquier fecha de este mes.
+*/
