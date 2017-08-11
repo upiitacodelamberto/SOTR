295675b867673d0687ecd17aa8136062df4a480a
root <root@localhost.localdomain>
2017-08-11 18:06:38 -0500

    2017.08.11.18.06

--- UNIDADES/U0/01_printf_xv6_borrowed/TestPrintf.c
+++ UNIDADES/U0/01_printf_xv6_borrowed/TestPrintf.c
@@ -9,11 +9,12 @@ extern void printf(int fd,char *fmt,...);
 extern void salir(int status);
 
 int main(){
-  char A[]="Hola Mundo C";
+  char A[]="Hello everybody!";
   printf(1,A);
   char B[]="\n";
   char C[]="SOTR grupo: %s\n";
   char D[]="3MV7 2017-B";
+  char E[]="Happy hackings!";
   printf(1,B);
   printf(1,"Viernes %d de agosto de %d.\n",11,2017);
   printf(1,B);
@@ -27,8 +28,10 @@ int main(){
   long c=(long)&a;
   long d=(long)&b;
   printf(1,"Notemos que la resta &a-&b=%d en aritmetica de apuntador.\n",&a-&b);
-  printf(1,"Pero si hacemos que c=(long)&a y que d=(long)&b,\n");
-  printf(1,"entonces la resta c-d=%d\n",c-d);
+  printf(1,"Mientras que la resta &b-&a=%d en aritmetica de apuntador.\n",&b-&a);
+  printf(1,"Pero si hacemos que c=(long)&a y que d=(long)&b, entonces:\n");
+  printf(1,"la resta c-d=%d\n",c-d);
+  printf(1,"y la resta d-c=%d\n%s\n",d-c,E);
   printf(1,B);
 
   salir(0xff);
